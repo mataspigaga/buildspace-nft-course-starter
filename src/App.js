94513e8cae156787a8c45246dfedd2dfe02503da
Matas Pigaga <tech@matas.io>
2022-02-17 11:32:28 -0500

    connect wallet feature done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,82 @@
 import "./styles/App.css";
 import { SiTwitter } from "react-icons/si";
-import React from "react";
+// import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import { useEffect, useState } from "react";
 
 // Constants
 const TWITTER_HANDLE = "mataspigaga";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-// const OPENSEA_LINK = '';
+// const OPENSEA_LINK = "";
 // const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  // const [darkMode, setDarkMode] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  // const darkModeHandler = () => {
+  //   setDarkMode((prevMode) => !prevMode);
+  // };
+
+  // First make sure we have access to window.ethereum
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    // Check if we're authorized to user's wallet
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
+
+  // This runs our function when the page loads.
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="App">
@@ -24,7 +86,13 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+            <button onClick={null} className="cta-button connect-wallet-button">
+              Mint NFT
+            </button>
+          )}
         </div>
         <div className="footer-container">
           <a
@@ -35,6 +103,9 @@ const App = () => {
           >
             <SiTwitter className="twitter-logo" />
           </a>
+          {/* <div onClick={darkModeHandler} className="darkMode-button">
+            {darkMode ? <BsFillSunFill /> : <BsFillMoonFill />}
+          </div> */}
         </div>
       </div>
     </div>
